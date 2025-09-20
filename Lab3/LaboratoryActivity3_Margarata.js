09a2fd8994249f6ad8f0ece2b84b4e8f5b729e83
mseaneric-git <229604890+mseaneric-git@users.noreply.github.com>
2025-09-21 01:20:48 +0800

    Bug fixes

--- Lab3/LaboratoryActivity3_Margarata.js
+++ Lab3/LaboratoryActivity3_Margarata.js
@@ -12,13 +12,13 @@ function calculateGrade(score){
 	if(score<60){
 		return 'F';
 	}
-	else if(score<=70){
+	else if(score<70){
 		return 'D';
 	}
-	else if(score<=80){
+	else if(score<80){
 		return 'C';
 	}
-	else if(score<=90){
+	else if(score<90){
 		return 'B';
 	}
 	else{
